fa0e80678b7250db5d0785861d1f1f35aefed5a2
kobe24u <kobe24u@vip.qq.com>
2017-03-26 10:50:35 +1100

    Initial Commit

---
+++ Carousel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Carousel
+//
+//  Created by Vincent Liu on 26/3/17.
+//  Copyright © 2017 200OK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
