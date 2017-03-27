22e4959e2347ed6b17ab784ffa6dc6e6f50cf83c
kobe24u <kobe24u@vip.qq.com>
2017-03-27 16:16:51 +1100

    Improved photo gallery

---
+++ Carousel/weather.swift
@@ -0,0 +1,9 @@
+//
+//  weather.swift
+//  Carousel
+//
+//  Created by Vincent Liu on 26/3/17.
+//  Copyright © 2017 200OK. All rights reserved.
+//
+
+import Foundation
---
+++ PhotoCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoCollectionViewCell.swift
+//  Carousel
+//
+//  Created by Vincent Liu on 26/3/17.
+//  Copyright © 2017 200OK. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    
+}
